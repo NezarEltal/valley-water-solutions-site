68cd180ebab28d7446f97794d4fa1f178f6b40e3
neltal <nezar.eltal@gmail.com>
2025-10-06 17:18:59 -0700

    all to master

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+    if (menuToggle) {
+      menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+      });
+    }
+  });
